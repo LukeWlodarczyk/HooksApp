81d6b838e5c9ce85f3557d1e79a8d47d59e2155a
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-10-30 23:49:43 +0100

    add custom hook - usePersistedState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,14 +1,33 @@
 import React, { useState } from 'react';
 
+const usePersistedState = (initialValue, name) => {
+  const [value, setValue] = useState(+localStorage.getItem(name) || initialValue);
+
+  return {
+    value,
+    setValue,
+  }
+
+}
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  const counter = usePersistedState(0, 'counter');
+
+  const increment = () => {
+    localStorage.setItem('counter', counter.value + 1);
+    counter.setValue(counter.value + 1);
+  }
+
+  const decrement = () => {
+    localStorage.setItem('counter', counter.value - 1);
+    counter.setValue(counter.value - 1);
+  }
 
   return (
     <div>
-      Count: {count}
-      <button onClick={() => setCount(count+1)}>Add one</button>
-      <button onClick={() => setCount(count-1)}>Minus one</button>
+      Count: {counter.value}
+      <button onClick={increment}>Add one</button>
+      <button onClick={decrement}>Minus one</button>
     </div>
   )
 }
