5bb174c7719c67eebdd846648ab93b17b7c3450d
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-11-01 00:00:36 +0100

    add code splitting using lazy and Suspense react feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 
 import Counter from './components/Counter';
+
+const LazyHello = lazy(() => import('./components/Hello'))
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App ">
        <Counter />
+       <Suspense fallback={<p>Loading</p>} >
+        <LazyHello />
+       </Suspense>
+     
       </div>
     );
   }
