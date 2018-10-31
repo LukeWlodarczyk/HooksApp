0face977c5f1290ddeb038eaedfd9c3867679440
LukeWlodarczyk <wlod_a_rczyk@interia.pl>
2018-11-01 00:23:43 +0100

    add react router to separate tested features

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component, Suspense, lazy } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
+import Header from './components/Header';
 import Counter from './components/Counter';
+import TodoList from './components/TodoList';
 
 const LazyHello = lazy(() => import('./components/Hello'))
 
@@ -8,11 +11,18 @@ class App extends Component {
   render() {
     return (
       <div className="App ">
-       <Counter />
-       <Suspense fallback={<p>Loading</p>} >
-        <LazyHello />
-       </Suspense>
-     
+        <BrowserRouter>
+          <>
+            <Header />
+            <Route exact path='/' component={Counter} />
+            <Route path='/lazy' render={() => 
+              <Suspense fallback={<p>Loading</p>} >
+                <LazyHello />
+              </Suspense>
+            } />
+            <Route path='/todos' component={TodoList} />
+          </>
+        </BrowserRouter>
       </div>
     );
   }
